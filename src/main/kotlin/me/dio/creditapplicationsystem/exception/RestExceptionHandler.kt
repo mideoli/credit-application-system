8aa4014af07cc2d627a2609dd99a69a0787a464e
Miguel de Oliveira <dmiurgosmiguel@gmail.com>
2024-02-26 19:22:28 +0200

    Handler Global: Tratando uma Exception Personalizada

--- src/main/kotlin/me/dio/creditapplicationsystem/exception/RestExceptionHandler.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/exception/RestExceptionHandler.kt
@@ -56,4 +56,34 @@ class RestExceptionHandler {
             ), HttpStatus.CONFLICT
         )*/
     }
+
+    @ExceptionHandler(BusinessException::class)
+    fun handlerValidException(ex: BusinessException): ResponseEntity<ExceptionDetails> {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(
+                ExceptionDetails(
+                    title = "Bad Request! Consult Documentation",
+                    timestamp = LocalDateTime.now(),
+                    status = HttpStatus.BAD_REQUEST.value(),
+                    exception = ex.javaClass.toString(),
+                    details = mutableMapOf(ex.cause.toString() to ex.message)
+                )
+            )
+    }
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handlerValidException(ex: IllegalArgumentException): ResponseEntity<ExceptionDetails> {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(
+                ExceptionDetails(
+                    title = "Bad Request! Consult Documentation",
+                    timestamp = LocalDateTime.now(),
+                    status = HttpStatus.BAD_REQUEST.value(),
+                    exception = ex.javaClass.toString(),
+                    details = mutableMapOf(ex.cause.toString() to ex.message)
+                )
+            )
+    }
 }
