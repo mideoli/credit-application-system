4a7d82c561c2b8460f0b75ee5d7f9f5fcbc179dd
Miguel de Oliveira <dmiurgosmiguel@gmail.com>
2024-02-26 15:12:09 +0200

    Implementa Endpoint CreditResource (save) e CreditDto

--- src/main/kotlin/me/dio/creditapplicationsystem/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/controller/CreditResource.kt
@@ -1,4 +1,23 @@
 package me.dio.creditapplicationsystem.controller
 
-class CreditResource {
+import me.dio.creditapplicationsystem.dto.CreditDto
+import me.dio.creditapplicationsystem.entity.Credit
+import me.dio.creditapplicationsystem.service.impl.CreditService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/api/credits")
+class CreditResource(
+    private val creditService: CreditService
+) {
+
+    @PostMapping
+    fun saveCredit(@RequestBody creditDto: CreditDto): String {
+        val credit: Credit = this.creditService.save(creditDto.toEntity())
+        return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+    }
 }
--- src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CreditDto.kt
@@ -1,2 +1,23 @@
-package me.dio.creditapplicationsystem.dto 
+package me.dio.creditapplicationsystem.dto
 
+import me.dio.creditapplicationsystem.entity.Credit
+import me.dio.creditapplicationsystem.entity.Customer
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class CreditDto(
+    val creditValue: BigDecimal,
+    val dayFirstInstallment: LocalDate,
+    val numberOfInstallments: Int,
+    val customerId: Long
+) {
+
+    fun toEntity(): Credit = Credit(
+
+        creditValue = this.creditValue,
+        dayFirstInstallment = this.dayFirstInstallment,
+        numberOfInstallments = this.numberOfInstallments,
+        customer = Customer(id = this.customerId)
+    )
+
+}
