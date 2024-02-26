f528196ffc36cb3503b4dba8d5d20d0926006ff7
Miguel de Oliveira <dmiurgosmiguel@gmail.com>
2024-02-26 12:19:40 +0200

    Implementacao da interface - CreditService (findByCreditCode)

--- src/main/kotlin/me/dio/creditapplicationsystem/repository/CreditRepository.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/repository/CreditRepository.kt
@@ -3,7 +3,9 @@ package me.dio.creditapplicationsystem.repository
 import me.dio.creditapplicationsystem.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
 interface CreditRepository: JpaRepository<Credit, Long> {
+    fun findByCreditCode(creditCode: UUID): Credit? //pode retornar um credit ou nao
 }
--- src/main/kotlin/me/dio/creditapplicationsystem/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/service/impl/CreditService.kt
@@ -22,7 +22,9 @@ class CreditService(
         TODO("Not yet implemented")
     }
 
-    override fun findByCreditCode(creditCode: UUID): Credit {
-        TODO("Not yet implemented")
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit: Credit = (this.creditRepository.findByCreditCode(creditCode) ?:
+            throw RuntimeException("Credit code: $creditCode not found"))
+        return if (credit.customer?.id == customerId) credit else throw RuntimeException("Contact admin")
     }
 }
